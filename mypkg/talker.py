5897620aa80b9c1d3a7c1b0f5cbf3a19e17fb1b3
Ryuta Kawamoto <ryu073000@i.softbank.jp>
2025-12-11 22:57:49 +0900

    lesson11の内容を行った

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -3,21 +3,22 @@ from rclpy.node import Node
 from std_msgs.msg import Int16
 
 class Talker():
-    def __init__(self):
-        self.pub = node.create_publisher(Int16,"countup",10)
+    def __init__(self, nh):
+        self.pub = nh.create_publisher(Int16,"countup",10)
         self.n = 0
+        nh.create_timer(0.5,self.cb)
 
-rclpy.init()
-node = Node("talker")
-talker = Talker()
-
-def cb():
-    msg = Int16()
-    msg.data = talker.n
-    talker.pub.publish(msg)
-    talker.n += 1
+    def cb(self):
+        msg = Int16()
+        msg.data = self.n
+        self.pub.publish(msg)
+        self.n += 1
 
 def main():
-    node.create_timer(0.5,cb)
+    rclpy.init()
+    node = Node("talker")
+    talker = Talker(node)
     rclpy.spin(node)
 
+if __name__ == '__main__':
+    main()
