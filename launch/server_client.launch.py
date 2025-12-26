ff59cb65eb5ad3576ae8136cb7e54ce66883299d
Ryuta Kawamoto <ryu073000@i.softbank.jp>
2025-12-27 01:28:50 +0900

    serverとclientに適したものに変更

--- launch/server_client.launch.py
+++ launch/server_client.launch.py
@@ -5,14 +5,14 @@ import launch_ros.actions
   
 def generate_launch_description():
   
-    talker = launch_ros.actions.Node(
+    server = launch_ros.actions.Node(
         package='mypkg',      #パッケージの名前を指定
-        executable='talker',  #実行するファイルの指定
+        executable='server',  #実行するファイルの指定
         )
-    listener = launch_ros.actions.Node(
+    client = launch_ros.actions.Node(
         package='mypkg',
-        executable='listener',
+        executable='client',
         output='screen'        #ログを端末に出すための設定
          )
  
-    return launch.LaunchDescription([talker, listener])   
+    return launch.LaunchDescription([server, client])   
