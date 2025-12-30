7a3b50fa86aa4b1a3c6bf8b07bd639cb174dd76b
Ryuta Kawamoto <ryu073000@i.softbank.jp>
2025-12-30 23:12:11 +0900

    編集

--- test/test.bash
+++ test/test.bash
@@ -2,34 +2,14 @@
 # SPDX-FileCopyrightText: 2025 Ryuta Kawamoto ryu073000@i.softbank.jp
 # SPDX-License-Identifier: BSD-3-Clause
 
-ng(){
-	echo ${1}行目が違うよ
-	res=1
-}
+dir=~
+[ "$1" != "" ] && dir="$1"
 
-res=0
-### NORMAL INPUT ###
-out=$(echo "林檎 100" | ./mkcon)
-[ "${out}" = 100.0 ] || ng "$LINENO"
+cd $dir/ros2_ws
+colcon build
+source $dir/.bashrc
 
-out=$(echo apple 10000 | ./mkcon)
-[ "$?" = 0 ] || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
+# Launchを実行してログをファイルに保存
+timeout 10 ros2 launch mypkg server_client.launch.py > /tmp/mypkg.log
 
-### ENPTY INPUT ###
-out=$(echo | ./mkcon)
-[ "$?" = 0 ] || ng "$LINENO"
-
-### STRANGE  INPUT ###
-out=$(echo a a 100 | ./mkcon)
-[ "$?" = 1 ] || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
-
-out=$(echo a a | ./mkcon)
-[ "$?" = 1 ] || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
-
-[ "${res}" = 0 ] && echo OK
-exit $res
-
-
+cat /tmp/mypkg.log | grep 'Login Successful'
